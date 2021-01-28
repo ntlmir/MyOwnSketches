2aaf36fcb75a382b20029553771136475080356c
ntlmir <49791963+ntlmir@users.noreply.github.com>
2021-01-28 12:36:35 +0500

    Add files via upload

---
+++ Tetris2.0/TETRIS/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TETRIS
+{
+    class Point
+    {
+        int x;
+        int y;
+
+        public Point (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void DrawPoint()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("\u00A4");
+        }
+
+        public void PointLight()
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+            Console.ResetColor();
+        }
+
+        public void FullPointLight()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Tetris2.0/TETRIS/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TETRIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Frame();
+
+            Massif MAS = new Massif();
+            MAS.CreateMassif();
+            MAS.MassifLight();
+
+            //Figures f1 = new Figures();
+            //f1.CreateFigure();
+
+            Console.ReadLine();
+        }
+
+        static void Frame()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Horisontal(2);
+            Vertical(3);
+            Horisontal(23);
+            Vertical(14);
+            Console.ResetColor();
+        }
+
+        static void Horisontal (int y)
+        {
+            for (int x=3; x<15; x++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(" ");
+                //Thread.Sleep(50);
+            }
+        }
+
+        static void Vertical (int x)
+        {
+            for (int y=3; y<24; y++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(" ");
+                //Thread.Sleep(50);
+            }
+        }
+    }
+}
